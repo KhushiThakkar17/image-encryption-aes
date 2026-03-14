ecae839558787834ccf119e7e01b535b9a3ff7f5
Khushi Thakkar <khushithakkar210@gmail.com>
2026-03-14 10:58:19 -0400

    Fixed image paths for portability

--- code.py
+++ code.py
@@ -163,7 +163,7 @@ tk.Label(root, text="Select Image File or Encrypted File:", bg="light yellow" , 
 image_entry = tk.Entry(root, width=40)
 image_entry.grid(row=0, column=1, padx=10, pady=10)
 
-img = Image.open("Browse_button.png")
+Image.open(os.path.join(BASE_DIR, "Browse_button.png"))
 resized_img = img.resize((24, 24), Image.LANCZOS)
 
 browse_icon = ImageTk.PhotoImage(resized_img)
@@ -176,7 +176,7 @@ password_entry.grid(row=1, column=1, padx=10, pady=10)
 
 # Encrypt Button and encryption icon
 
-img = Image.open("Encrypt_button.png")
+img = Image.open(os.path.join(BASE_DIR, "Encrypt_button.png"))
 resized_img = img.resize((24, 24), Image.LANCZOS)
 Encrypt_icon = ImageTk.PhotoImage(resized_img)
 Encrypt_button = tk.Button(root, image=Encrypt_icon, text="Encrypt", bg="tan", compound="left", command=encrypt_button_action)
@@ -184,7 +184,7 @@ Encrypt_button.grid(row=2, column=1, padx=10, pady=10, sticky='w')
 
 # Decrypt Button and decryption icon
 
-img = Image.open("Decrypt_button.png")
+img = Image.open(os.path.join(BASE_DIR, "Decrypt_button.png"))
 resized_img = img.resize((24, 24), Image.LANCZOS)
 Decrypt_icon = ImageTk.PhotoImage(resized_img)
 Decrypt_button = tk.Button(root, image=Decrypt_icon, text="Decrypt", bg="tan", compound="left", command=decrypt_button_action)
